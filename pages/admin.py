984de4912ab5df67fe1b9bd8b0670ae6f47c32e8
ninos77 <ninos_zaiya@hotmail.com>
2021-02-05 23:48:37 +0100

    Make new admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import Team, BusinessInfo
+from django.utils.html import format_html
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="35" style="border-radius: 50%" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'photo'
+
+    list_display = ['full_name', 'thumbnail', 'designation', 'created_date']
+    list_display_links = ('full_name', 'thumbnail')
+
+    class Meta:
+        model = Team
+
+class BusinessInfoAdmin(admin.ModelAdmin):
+  
+  list_display = ['name_title', 'phone_number', 'email_address', 'opening_hours']
+
+
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(BusinessInfo, BusinessInfoAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import BusinessInfo
 
-# Create your views here.
+# Cdef home(request):
+
+def home(request):
+  return render(request, 'pages/home.html')
+
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+def aboutBusiness(request):
+    busines_info = BusinessInfo.objects.all()
+    return render(request, 'templates/includes/main-nav.html', {'busines_info': busines_info})
